6a4fa4ce9a7d06438bf99c42ed552e82b57b46f8
chun-andrew <49577025+chun-andrew@users.noreply.github.com>
2022-04-11 13:39:38 -0400

    added redirectURI
    added redirect URI for hosting locally

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ retrieveToken();
 async function retrieveToken() {
 	const clientID = "95a3053093854b948e0e7ab02218af28"; //my personal clientID
 	const clientSecret = "155e279a6ac7416f84833a1f99bd1764"; //my personal client Secret
+	const redirectURI = "http://localhost:8888/callback"; //redirect URI for local environment
 
 	const result = await fetch("https://accounts.spotify.com/api/token", {
 		method: "POST",
