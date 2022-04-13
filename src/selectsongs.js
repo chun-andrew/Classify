262d7b586a1db32ed7d69215e6f1ab78f33f208f
Lucy <lucyadhill@gmail.com>
2022-04-12 23:40:49 -0400

    worked on JS a for selectsongs

---
+++ src/selectsongs.js
@@ -0,0 +1,13 @@
+
+const axios = require('axios');
+
+//list of playlists the the user chose for new playlist **will update to value from chooseplaylists**
+
+const chosenPlaylists = [];
+
+var allSongs = [];
+
+for(i=0; i < chosenPlaylists.length; i++){ 
+    
+}
+
