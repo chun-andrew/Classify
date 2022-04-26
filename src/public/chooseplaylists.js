6df0794f73d4fbf4a9b9b8d68cbc59ee9053ce72
chun-andrew <49577025+chun-andrew@users.noreply.github.com>
2022-04-25 22:50:28 -0400

    Add Playlists
    Save usertoken in local storage, display how many playlists a user has

---
+++ src/public/chooseplaylists.js
@@ -0,0 +1,14 @@
+// import { getUserAccessToken } from "../main.js";
+
+// const main_func = require("../main");
+// var express = require("express"); // Express web server framework
+// var request = require("request"); // "Request" library
+// var cors = require("cors");
+// var querystring = require("querystring");
+// var cookieParser = require("cookie-parser");
+
+// var userAccessToken =exports.userAccessToken
+// var user = exports.getUser
+let userAccessToken = localStorage.getItem("accessToken");
+console.log("test: user access token is ");
+console.log(userAccessToken);
